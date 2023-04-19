42e4afdf336ca461fbe7ea600a3561a1d1c3f74e
Viktoria Hillerud <viktoriahillerud@gmail.com>
2023-04-19 19:20:50 +0200

    deploying

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -214,6 +214,7 @@ const changeUnit = () => {
                          <p>{Math.round((data.main.temp - 32) / 9/5) }&deg;C</p>
                       )}
                        
+                       
                       </div>
                   </div>
                 </div>
