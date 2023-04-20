31ae5d4c87065b63859263e89cb0cf0e6b143d41
Viktoria Hillerud <viktoriahillerud@gmail.com>
2023-04-20 12:38:22 +0200

    edited kinda lot

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,13 +1,12 @@
 import { useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faXmark, faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
+import { faXmark, faSun, faMoon, faWind } from "@fortawesome/free-solid-svg-icons";
 
 import { useState } from "react";
 
 import Nav from "./components/nav/Nav";
 
 function App() {
-
   //Setting coords with geoLocation
   const [lat, setLat] = useState(null);
   const [lng, setLng] = useState(null);
@@ -15,7 +14,6 @@ function App() {
   const [weather, setWeather] = useState({});
   const [forecast, setForecast] = useState(undefined);
   const [celcius, setCelcius] = useState(true);
- 
 
   //Menu for showing one day forecast hour-by-hour
   const [menu, setMenu] = useState(false);
@@ -29,27 +27,29 @@ function App() {
       const urlForecast = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=38de3cc8678599c25ca4c9800d971a8b`;
 
       if (!navigator.geolocation) {
-        setStatus(
-          "We couldnt find your position!"
-        );
+        setStatus("We couldnt find your position!");
       } else {
         setStatus("");
       }
-      navigator.geolocation.getCurrentPosition((position) => {
-        setLat(position.coords.latitude);
-        setLng(position.coords.longitude);
-        console.log(position);
-      }, () => {
-        setStatus('Unable to retrieve position. Try search for your city instead!')
-      });
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLat(position.coords.latitude);
+          setLng(position.coords.longitude);
+          console.log(position);
+        },
+        () => {
+          setStatus(
+            "Unable to retrieve position. Try search for your city instead!"
+          );
+        }
+      );
 
       const responses = await Promise.all([
         fetch(urlWeather),
         fetch(urlForecast),
       ])
 
-
-      .then(res => Promise.all(res.map(r => r.json())))
+        .then((res) => Promise.all(res.map((r) => r.json())))
         .then((result) => {
           console.log(result);
           setWeather(result[0]);
@@ -61,16 +61,15 @@ function App() {
     getLocation();
   }, [lat, lng]);
 
-const changeUnit = () => {
- setCelcius(!celcius)
-}
-
+  const changeUnit = () => {
+    setCelcius(!celcius);
+  };
 
   return (
     //handleSubmit={handleSubmit} handleChange={handleChange} on Nav-component?
     <>
       <Nav />
-    
+
       <div className="App">
         <div className="container text-center">
           <div className="dailyholder row justify-content-md-center">
@@ -82,16 +81,19 @@ const changeUnit = () => {
               {typeof weather.main != "undefined" ? (
                 <div>
                   <h2>{weather.name}</h2>
-                  <button onClick={changeUnit} className="setDegree">Change degree</button>
-                  {!celcius && (
-                  <p>{Math.round(weather.main.temp)} &#176;K</p>
-                  )}
+                  <button onClick={changeUnit} className="setDegree">
+                    Change degree
+                  </button>
+                  {!celcius && <p>{Math.round(weather.main.temp)} &#176;K</p>}
                   {celcius && (
-                  <p>{Math.round((weather.main.temp) - 273.15) } &#176;C</p>
+                    <p>{Math.round(weather.main.temp - 273.15)} &#176;C</p>
                   )}
-                  wind
+                 
                   <p>
-                    speed{weather.wind.speed} deg{weather.wind.deg}
+                  <div>
+                      <FontAwesomeIcon icon={faWind} size="2xl"/>
+                    </div>
+                    speed {weather.wind.speed} deg {weather.wind.deg}
                   </p>
                   humidity
                   <p>{weather.wind.speed}%</p>
@@ -125,7 +127,7 @@ const changeUnit = () => {
                       <FontAwesomeIcon icon={faMoon} size="3x" />
                     </div>
                     <div>
-                    <h4>Sunset</h4>
+                      <h4>Sunset</h4>
                       {new Date(weather.sys.sunset * 1000).toLocaleTimeString(
                         "en-IN"
                       )}
@@ -139,57 +141,47 @@ const changeUnit = () => {
           </div>
         </div>
 
-        
-
-              {/* Forecast 5 days */}
+        {/* Forecast 5 days */}
 
         {menu && (
           <div>
             <div onClick={handleMenuShow}></div>
-            <div className="modalStyle" id="overlay ">
-              <div className="xmark">
+              <div className="modalStyle" id="overlay ">
+                <div className="xmark">
                 {" "}
-                <FontAwesomeIcon
+                 <FontAwesomeIcon
                   icon={faXmark}
                   style={{ cursor: "pointer" }}
                   size="3x"
                   onClick={handleMenuShow}
                 />
               </div>
-             
-              <table className="table table-hover">
-                <thead>
-                  <tr>
-                    <th scope="col"> </th>
-                    <th scope="col">Temp</th>
-                    <th scope="col">wind</th>
-                    <th scope="col">humidity</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">kl.</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">kl.</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">kl. </th>
-                    <td colSpan="2">Larry the Bird</td>
-                    <td>@twitter</td>
-                  </tr>
-                </tbody>
-              </table>
+              {forecast && (
+                <div className="d-flex justify-content-center p-2">
+                  {forecast.filter(fc => fc.dt_txt)
+                    .map((data) => {
+                      return (
+                        <div className="p-2">
+                          <table>
+                            <tr>
+                              <th>{data.dt_txt}</th>
+                            </tr>
+                            <tr>
+                              <td>   {!celcius && <p>{Math.round(weather.main.temp)} &#176;K</p>}
+                  {celcius && (
+                    <p>{Math.round(weather.main.temp - 273.15)} &#176;C</p>
+                  )}</td>
+                            </tr>
+                          </table>
+                        </div>
+                      );
+                    })}
+                </div>
+              )}
+         
             </div>
           </div>
         )}
-
 
         <div className="container text-center">
           <div className=" row justify-content-md-center">
@@ -198,36 +190,35 @@ const changeUnit = () => {
               className="cont  col col-sm-10 shadow-lg p-3 mb-5 bg-body-tertiary rounded"
             >
               5-day-prognosis
-              
               {forecast && (
-               <div className="d-flex justify-content-center p-2">
-              {forecast.filter(fc => fc.dt_txt.slice(11,19) === "09:00:00").map((data) => {
-          
-                    return (
-                      <div className="p-2">
-                      <div className="card " onClick={handleMenuShow}>
-                      <div className="card-body">
-                      <p>{data.dt_txt}</p>
-                      {!celcius && (
-                        <p>{Math.round(data.main.temp)}&deg;K</p>
-                      )}
-                      {celcius && (
-                         <p>{Math.round((data.main.temp) - 273.15) }&deg;C</p>
-                      )}
-                       
-                       
-                      </div>
-                  </div>
+                <div className="d-flex justify-content-center p-2">
+                  {forecast
+                    .filter((fc) => fc.dt_txt.slice(11, 19) === "09:00:00")
+                    .map((data) => {
+                      return (
+                        <div className="p-2">
+                          <div className="card " onClick={handleMenuShow}>
+                            <div className="card-body">
+                              <p>{data.dt_txt}</p>
+                              {!celcius && (
+                                <p>{Math.round(data.main.temp)}&deg;K</p>
+                              )}
+                              {celcius && (
+                                <p>
+                                  {Math.round(data.main.temp - 273.15)}&deg;C
+                                </p>
+                              )}
+                            </div>
+                          </div>
+                        </div>
+                      );
+                    })}{" "}
                 </div>
-                    )
-                  })} </div>
-                  
-                  ) }
-                    </div>
-                </div>
-              </div>
+              )}
             </div>
-          
+          </div>
+        </div>
+      </div>
     </>
   );
 }
